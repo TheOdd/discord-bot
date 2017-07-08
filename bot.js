847c67f1b6ba06834a5a5f4fb6dbf255be97478b
Owen Hines <owenhines07@gmail.com>
2017-07-08 01:07:34 -0700

    Help, compareMessage, coin flip, 8ball

--- bot.js
+++ bot.js
@@ -2,22 +2,64 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '+';
+const eightBallResponses = ['Odds aren\'t good.', 'No.', 'It will pass.', 'Cannot tell now.',
+  'You\'re hot.', 'Count on it.', 'Bet on it.', 'Maybe', 'Possibly',
+  'Ask again.', 'No doubt.', 'Absolutely', 'Very likely.', 'Act now.',
+  'Stars say no.', 'Can\'t say.', 'Not now.', 'Go for it.', 'Yes.', 'It\'s O.K.'
+];
 
 function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function compareMessage(message, command) {
+  var messageSplit = message.content.split(' ')[0];
+  return messageSplit.toUpperCase() === prefix + command.toUpperCase();
+}
 
 client.on('ready', () => {
   console.log('Bot is ready.');
+
+  // Setting bot's game to "+HELP"
+  client.user.setPresence({
+    status: 'online',
+    game: {
+      name: '+HELP'
+    }
+  });
 });
 
 client.on('message', message => {
-  if (message.content.toUpperCase() === prefix + 'nudes'.toUpperCase()) {
+
+  // +HELP
+  if (compareMessage(message, 'help')) {
+    message.channel.send('**Commands:**\n\n **Help:** Display this menu.\n **Nudes:** Get some spicy nudes.\n **Flip** / **Coin:** Flip a coin!\n **8Ball:** Ask the magic 8-Ball a question!\n\n *All commands must have a + before them to be registered!*');
+  }
+
+  // +NUDES
+  else if (compareMessage(message, 'nudes')) {
     var picPath = 'nudes/nudes-' + randomIntFromInterval(1, 11) + '.png';
     message.channel.send('Here are some spicy nudes, just like you asked.', {
       file: picPath
     });
+  }
+
+  // +FLIP or +COIN
+  else if (compareMessage(message, 'flip') || compareMessage(message, 'coin')) {
+    var coinVal = randomIntFromInterval(0, 1);
+    // 0 - HEADS
+    // 1 - TAILS
+    if (coinVal === 0) {
+      message.channel.send('Heads!');
+    } else if (coinVal === 1) {
+      message.channel.send('Tails!');
+    }
+  }
+
+  // +8Ball
+  else if (compareMessage(message, '8ball')) {
+    var responseNum = randomIntFromInterval(0, eightBallResponses.length);
+    message.channel.send(eightBallResponses[responseNum]);
   }
 });
 
