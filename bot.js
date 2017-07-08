c61b8fd768e3856e0bd9d34367d9508af6fdd702
Owen Hines <owenhines07@gmail.com>
2017-07-08 00:33:13 -0700

    Added 'nudes' command. Added welcome message.

--- bot.js
+++ bot.js
@@ -1,15 +1,28 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '+';
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 
 client.on('ready', () => {
   console.log('Bot is ready.');
 });
 
 client.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong');
+  if (message.content.toUpperCase() === prefix + 'nudes'.toUpperCase()) {
+    var picPath = 'nudes/nudes-' + randomIntFromInterval(1, 11) + '.png';
+    message.channel.send('Here are some spicy nudes, just like you asked.', {
+      file: picPath
+    });
   }
+});
+
+client.on('guildMemberAdd', member => {
+  member.guild.defaultChannel.send(`Welcome to the server, ${member}!`);
 });
 
 client.login(process.env.DISCORD_TOKEN);
